cdf4708d76d011625fa3ecf806314c2107962964
Soichiro Yoshimura <yoshimura@soichiro.org>
2019-05-17 10:34:38 +0900

    Start implement checkTweetable

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,28 @@ exports.createGoogleUserJWT = functions.https.onCall((data, context) => {
   const user = context.auth;
   const email = context.auth.token.email;
   console.log(user);
+
+  const token = jwt.sign(user, secret);
+  return {token: token, email : email};
+});
+
+exports.checkTweetable = functions.https.onCall((data, context) => {
+  const twitterUser = context.auth;
+  const twitterUID = context.auth.token.providerData[0].uid;
+  console.log(twitterUser);
+
+  const googleUserToken = data.token;
+  const googleUser = jwt.verify(googleUserToken, secret);
+  const googleEmail = data.email;
+
+  if(googleUser.token.email !== googleEmail) {
+    return { 
+      tweetable : false,
+      reason : 'INVALID_EMAIL'
+    }
+  }
+
+
   const token = jwt.sign(user, secret);
   return {token: token, email : email};
 });
